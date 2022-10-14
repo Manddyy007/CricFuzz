cd790bdd16a737a2ac3fc3cd82756a465ffc0540
Mandar <83168259+code-funda@users.noreply.github.com>
2022-10-14 12:45:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $('#getDogButton').click(function(){
-      $.get("https://cricapi.com/api/cricket?apikey=sWrrPaGxxMh8m4ytbFuzKF21oBi1 ", function(matchdata) {
+      $.get("https://cricapi.com/api/cricket?apikey=your_API_Key", function(matchdata) {
           var i=0;
 	matchdata.data.forEach(function(md) {
 		console.log(md.description + " - to grab more details, simply use the unique_id " + md.unique_id + " with the cricketScore api!");   
